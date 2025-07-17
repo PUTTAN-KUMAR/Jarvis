8a9575484f9b5b744b9c1f86dc2d59b696f5e0a1
puttan kumar <puttankumar76@gmail.com>
2025-07-18 00:48:39 +0530

    add new file

---
+++ musicLibrary.py
@@ -0,0 +1,16 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "one": "https://www.youtube.com/watch?v=AggDciJh5Iw",
+    "3-idiots": "https://www.youtube.com/watch?v=G7KtPzUpCAc",
+    "Bande": "https://www.youtube.com/watch?v=v4zQ_YJfSn8",
+    "kgf": "https://www.youtube.com/watch?v=5CP9fycq8pk",
+    "marjaavaan": "https://www.youtube.com/watch?v=q88b2MFHal4&list=RDq88b2MFHal4&start_radio=1&rv=5CP9fycq8pk",
+    "kalank": "https://www.youtube.com/watch?v=Grr0FlC8SQA&list=RDq88b2MFHal4&index=4",
+    "dhoni":"https://www.youtube.com/watch?v=Ov0YGGSY6gY&list=RDq88b2MFHal4&index=15",
+    "rahat-ali": "https://www.youtube.com/watch?v=QW_5uoG2q90&list=RDq88b2MFHal4&index=17&pp=8AUB"
+
+
+}
